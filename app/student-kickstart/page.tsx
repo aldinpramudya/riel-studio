d3120d87b2a801e83156d5a769f23abfe79f6037
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-28 21:56:04 +0700

    feat : kickstart page card

---
+++ app/student-kickstart/page.tsx
@@ -0,0 +1,28 @@
+export default function StudentKickstart() {
+    return (
+        <>
+            <div className="px-50">
+                {/* Card Welcome Student Kickstart */}
+                <div className="w-full px-10 py-10 text-[#efeff9] rounded-lg bg-linear-to-r from-[#FF4F04] via-[#FFA743] to-[#F68554] shadow-sm">
+                    <p className = "font-bold text-[20px]">
+                        Kickstart Your Ideas, Even Before Graduation. with
+                    </p>
+                    {/* Tagline */}
+                    <h1 className="font-bold text-[96px]">
+                        #StudentKickstart
+                    </h1>
+                    {/* Tagline End */}
+                    <p className="text-[20px]">
+                        Because every great journey starts with a spark <br />
+                        RIEL.studio here to help students and college creators turn their concepts into something real.
+                    </p>
+                </div>
+                {/* Card Welcome End */}
+                {/* What is this */}
+                
+
+                {/* What is this End */}
+            </div>
+        </>
+    )
+}
