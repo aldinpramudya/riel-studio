6066b57a2b228cec659d5f8925dd41b7629af7e4
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-28 09:34:18 +0700

    feat : fixing card portfolio component

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,7 @@
 // Components
 import Button from "@/components/button"
 import CardPortfolio from "@/components/cardPortfolio";
-import PillBadges from "@/components/pillBadges";
+import CardTestimonies from "@/components/cardTestimonies";
 
 // Icons
 import { IoCall } from "react-icons/io5";
@@ -24,11 +24,37 @@ export default function Portfolio() {
                     {/* Title End*/}
                 </div>
                 {/* Portfolio Done */}
-                <div className="pt-15">
-                    <CardPortfolio portfolioTitle="Project Title" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna."  />
-
+                <div className="pt-15 flex flex-row flex-wrap space-x-5 space-y-3 justify-center">
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+                    <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
                 </div>
                 {/* Portfolio Done End */}
+                {/* Testimonies */}
+                <div className="pt-15">
+                    {/* Typography */}
+                    <div className="text-center">
+                        <p className="capitalize font-bold text-[64px] tracking-tight bg-linear-to-r from-[#FF4F04] via-[#FFA743] to-[#F68554] bg-clip-text text-transparent">
+                            Hear it From Our Clients
+                        </p>
+                        <p className="font-bold text-[24px]">
+                            They Started. They Built. They Spoke
+                        </p>
+                    </div>
+                    {/* Typography End */}
+                    {/* Cards Testimonies */}
+                    <div className="flex justify-center space-x-2 pt-15">
+                        <CardTestimonies testimonyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a mauris neque. Sed id dolor sed velit maximus tincidunt a at est. Vestibulum semper nisl ac suscipit consectetur" name="Riel" position="Student At Lala Land" />
+                        <CardTestimonies testimonyText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a mauris neque. Sed id dolor sed velit maximus tincidunt a at est. Vestibulum semper nisl ac suscipit consectetur" name="Riel" position="Student At Lala Land" />
+                    </div>
+                    {/* Card Testimonies */}
+                </div>
+                {/* Testimonies End */}
                 {/* CTA */}
                 <div className="pt-20">
                     {/* Typography */}
--- components/cardPortfolio.tsx
+++ components/cardPortfolio.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 import Button from "@/components/button";
+import { RxOpenInNewWindow } from "react-icons/rx";
+import { FiGithub } from "react-icons/fi";
 
 interface CardPortfolioProps {
     portfolioTitle: string;
@@ -15,26 +17,42 @@ export default function CardPortfolio({
     portfolioDescription,
     portfolioTech = "",
     portfolioLiveDemoLink = "",
-    portfolioGithubLink="",
+    portfolioGithubLink = "",
 }: CardPortfolioProps) {
     return (
         <div className="border border-(--main-color) rounded-lg w-[400px]">
             <div>
-
+                <Image 
+                    src="/images/bg-main-color.png"
+                    alt="Projects Photo"
+                    width={400}
+                    height={200}
+                    className="rounded-lg border border-b-(--main-color)"
+                />
             </div>
+            {/* Typography */}
             <div className="px-10 py-10">
                 <p className="text-[24px] font-bold">
                     {portfolioTitle}
                 </p>
-                <p>
+                <p className="text-justify">
                     {portfolioDescription}
                 </p>
                 <p className="pt-3">
                     {portfolioTech}
                 </p>
             </div>
-            {portfolioLiveDemoLink},
-            {portfolioGithubLink},
+            {/* Typography End */}
+            {/* Buttons*/}
+            <div className="px-10 pb-10 space-x-3">
+                <a href={portfolioLiveDemoLink} target="_blank">
+                    <Button label="Live Demo" labelClassName="text-[14px]" icon={<RxOpenInNewWindow />} />
+                </a>
+                <a href={portfolioGithubLink} target="_blank">
+                    <Button label="Github" variant="secondary" labelClassName="text-[14px]" icon={<FiGithub />} />
+                </a>
+            </div>
+            {/* Buttons End */}
         </div>
     )
 }
