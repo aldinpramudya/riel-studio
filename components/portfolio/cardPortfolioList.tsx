a0e0e288e6587825eb2cbbbbb28c93b373a38c7e
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-30 21:28:20 +0700

    feat : layout mobile portfolio

--- components/portfolio/cardPortfolioList.tsx
+++ components/portfolio/cardPortfolioList.tsx
@@ -4,9 +4,7 @@ export default function CardPortfolioList() {
     return (
         <>
             {/* Portfolio Done */}
-            <div className="pt-15 flex flex-row flex-wrap space-x-5 space-y-3 justify-center">
-                <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
-                <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
+            <div className="flex justify-center pt-15 md:gap-10 gap-10 flex-wrap ">
                 <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
                 <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
                 <CardPortfolio portfolioTitle="Project Title" portfolioLiveDemoLink="https://github.com/aldinpramudya" portfolioDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur, velit vitae faucibus ornare, eros sapien venenatis magna." />
